70f4467faef391c4c9a0eefec7cd7fbbd4a08131
mmoya26 <mmoya18@icloud.com>
2020-08-04 22:46:16 -0500

    Add initial Xcode files

---
+++ Corona Now/Corona Now/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Corona Now
+//
+//  Created by Miguel Moya on 8/4/20.
+//  Copyright © 2020 Miguel Moya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
